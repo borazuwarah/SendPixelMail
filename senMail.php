3a92f6c016a56210d869804d07d03f24acfa43cf
Pablo Ramírez <yeyoycareto@gmail.com>
2021-01-28 14:46:04 +0100

    variablesprivadas y acceso a estas

--- senMail.php
+++ senMail.php
@@ -1,6 +1,21 @@
 <?php
+require('variablesprivadas.php');
+require('class.phpmailer.php');
+$mail = new PHPMailer();
+$mail -> From =("");//mail send
+$mail -> FromName ="Mi nombre";//nombre de la direccion que envia
+$mail -> AddAddress =("");//direccion de destino
+//$mail->WordWrap = 50; 
+$mail->IsHTML(true); 
 
-echo "Hola";
+
+
+if($mail->send()){
+    echo 'Message has been sent';
+}else{
+    echo 'Message could not be sent.';
+    echo 'Mailer Error: ' . $mail->ErrorInfo;
+}
 
 ?>
 
